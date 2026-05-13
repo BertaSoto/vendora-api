8e048961a677b898376b09a05753246b45f6698e
Berta Soto <bsotojerez@gmail.com>
2026-05-13 12:59:36 -0400

    fix: preparar monorepo para deploy de microservicios en vercel

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '../prisma/generated/client'
+import 'dotenv/config'
+import { PrismaClient } from '../../../prisma/generated/client.js'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL })
